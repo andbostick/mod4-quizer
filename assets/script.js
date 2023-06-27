8eecba555c2e3b30c6c23719fa42cf6a6688cd80
andrew bostick <andbostick@gmail.com>
2023-06-27 18:57:04 -0400

    scoring functionality and timer

--- assets/script.js
+++ assets/script.js
@@ -44,67 +44,84 @@ var questionsArr = [
 ];
 
 var currentQuestion = 0;
+var score = 0;
+var timeSpent = 0;
+var currentTime = 50;
 
+var timer = document.querySelector("#timer");
 var start = document.querySelector("#start");
 var nextButton = document.querySelector("#next");
 var questionContainer = document.querySelector("#question-container");
 var questionTitle = document.querySelector("#question-title");
 var questionAnswers = document.querySelector("#answer-buttons");
-var incorrectTitle = document.querySelector('#incorrect')
-var correctTitle = document.querySelector('#correct')
+var incorrectTitle = document.querySelector("#incorrect");
+var correctTitle = document.querySelector("#correct");
+var scoreForm = document.querySelector("section");
+var main = document.querySelector("main");
+var highScore = document.querySelector("#score");
 
 start.addEventListener("click", startGame);
-nextButton.addEventListener("click", nextQuestion)
+nextButton.addEventListener("click", nextQuestion);
 
 function startGame() {
   currentQuestion = 0;
   start.classList.add("hidden");
   questionContainer.classList.remove("hidden");
   nextQuestion();
+  startTimer();
 }
 
 function nextQuestion() {
   clearPrevQuestion();
   showQuestion(questionsArr[currentQuestion]);
-  currentQuestion++
-  
+  currentQuestion++;
 }
 
 function clearPrevQuestion() {
   nextButton.classList.add("hidden");
-  correctTitle.classList.add("hidden")
-  incorrectTitle.classList.add('hidden')
+  correctTitle.classList.add("hidden");
+  incorrectTitle.classList.add("hidden");
   questionAnswers.innerText = "";
 }
 
 function chooseAnswer(event) {
   var selected = event.target;
   var correctAns = selected.classList.contains("true");
-  console.log(correctAns)
+  console.log(correctAns);
   if (correctAns) {
-    correctTitle.classList.remove('hidden')
+    correctTitle.classList.remove("hidden");
+    score++;
   } else {
-    incorrectTitle.classList.remove('hidden')
+    incorrectTitle.classList.remove("hidden");
+    timeSpent += 10;
   }
-  
-    nextButton.classList.remove('hidden')
-    
-  
+
+  nextButton.classList.remove("hidden");
 }
 
-
-
 function showQuestion(question) {
-  
   questionTitle.innerHTML = question.question;
   question.answers.forEach((answer) => {
     var button = document.createElement("button");
     button.innerText = answer.text;
-      button.classList.add("buttons");
-      button.classList.add(`${answer.correct}`)
+    button.classList.add("buttons");
+    button.classList.add(`${answer.correct}`);
     button.addEventListener("click", chooseAnswer);
     questionAnswers.appendChild(button);
   });
 }
 
-console.log(currentQuestion)
+function startTimer() {
+  console.log(timeSpent);
+  timer.textContent = currentTime;
+  setTimer = setInterval(function () {
+    timeSpent++;
+    timer.textContent = currentTime - timeSpent;
+    if (timeSpent == 50) {
+      clearInterval(setTimer);
+      main.classList.add("hidden");
+      scoreForm.classList.remove("hidden");
+      highScore.innerText = score;
+    }
+  }, 1000);
+}
